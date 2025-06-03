2112dd68ac54eee684822d64658b663ab67f8856
Marco Dave <marcodave03@gmail.com>
2025-06-03 13:34:43 +0700

    revert back

--- src/main.tsx
+++ src/main.tsx
@@ -1,43 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import App from "./App";
-// import LandingPage from "./pages/LandingPage";
-// import "./App.css";
-// import { WalletProvider } from "@suiet/wallet-kit";
-// import ProtectedRoute from './components/ProtectedRoute';
-// import { GoogleOAuthProvider } from "@react-oauth/google";
-
-// const GOOGLE_CLIENT_ID = "990468881493-c98ech5p3srgtur82p33g6ckvdoucikn.apps.googleusercontent.com";
-
-// const root = ReactDOM.createRoot(
-//   document.getElementById("root") as HTMLElement
-// );
-
-// root.render(
-//   <React.StrictMode>
-//     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-//       <WalletProvider>
-//         <Router>
-//           <Routes>
-//             <Route path="/landing" element={<LandingPage />} />
-//             <Route 
-//               path="/" 
-//               element={
-//                 <ProtectedRoute>
-//                   <App interview_prompt="You are my girlfriend" />
-//                 </ProtectedRoute>
-//               } 
-//             />
-//           </Routes>
-//         </Router>
-//       </WalletProvider>
-//     </GoogleOAuthProvider>
-//   </React.StrictMode>
-// );
-
-
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -45,16 +5,10 @@ import App from "./App";
 import LandingPage from "./pages/LandingPage";
 import "./App.css";
 import { WalletProvider } from "@suiet/wallet-kit";
-import ProtectedRoute from "./components/ProtectedRoute";
+import ProtectedRoute from './components/ProtectedRoute';
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const GOOGLE_CLIENT_ID =
-  "990468881493-c98ech5p3srgtur82p33g6ckvdoucikn.apps.googleusercontent.com";
-
-// Add session check logic
-const isSessionReady =
-  localStorage.getItem("zkloginWallet")?.startsWith("0x") ||
-  localStorage.getItem("bypassWallet")?.startsWith("0x");
+const GOOGLE_CLIENT_ID = "990468881493-c98ech5p3srgtur82p33g6ckvdoucikn.apps.googleusercontent.com";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -65,23 +19,17 @@ root.render(
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
       <WalletProvider>
         <Router>
-          {isSessionReady ? (
-            <Routes>
-              <Route path="/landing" element={<LandingPage />} />
-              <Route
-                path="/"
-                element={
-                  <ProtectedRoute>
-                    <App interview_prompt="You are my girlfriend" />
-                  </ProtectedRoute>
-                }
-              />
-            </Routes>
-          ) : (
-            <div className="text-white flex justify-center items-center h-screen">
-              ⏳ Initializing session...
-            </div>
-          )}
+          <Routes>
+            <Route path="/landing" element={<LandingPage />} />
+            <Route 
+              path="/" 
+              element={
+                <ProtectedRoute>
+                  <App interview_prompt="You are my girlfriend" />
+                </ProtectedRoute>
+              } 
+            />
+          </Routes>
         </Router>
       </WalletProvider>
     </GoogleOAuthProvider>
